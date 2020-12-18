680e9f898173cc8d6829a8cf922ee07c88739e0b
Nikvau <75840361+Nikvau@users.noreply.github.com>
2020-12-18 14:57:55 +0300

    Add files via upload

---
+++ 2-4 (V2).pas
@@ -0,0 +1,18 @@
+﻿program a4;
+var
+  a,b,c,d: real;
+begin
+  writeln('Введите 4 числа: ');
+  read(a,b,c,d);
+
+  if (a <= b) and (b <= c) and (c <= d) then 
+     (a,b,c):=(d,d,d) 
+  else 
+    if not ((a > b) and(b > c) and (c > d)) then
+     (a,b,c,d):=(a*a,b*b,c*c,d*d);
+    
+  writeln('A= ',a);
+  writeln('B= ',b);
+  writeln('C= ',c);
+  writeln('D= ',d);
+end.
